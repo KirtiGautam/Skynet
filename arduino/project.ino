5b8725354dc0b35c24962e9a73234cefe880df1f
dev621 <vratgautam@gmail.com>
2021-10-31 21:59:33 +0530

    chamges

--- arduino/project.ino
+++ arduino/project.ino
@@ -90,36 +90,7 @@ void loop(){
             }
             
            
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-          
-            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
-            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println(".button2 {background-color: #77878A;}</style></head>");
             
-            // Web Page Heading
-            client.println("<body><h1>ESP8266 Web Server</h1>");
-            
-            // Display current state, and ON/OFF buttons for GPIO 5  
-            client.println("<p>GPIO 15 - State " + output15State + "</p>");
-            // If the output5State is off, it displays the ON button       
-            if (output15State=="off") {
-              client.println("<p><a href=\"/15/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/15/off\"><button class=\"button button2\">OFF</button></a></p>");
-            } 
-               
-            // Display current state, and ON/OFF buttons for GPIO 4  
-            client.println("<p>GPIO 14 - State " + output14State + "</p>");
-            // If the output4State is off, it displays the ON button       
-            if (output14State=="off") {
-              client.println("<p><a href=\"/14/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/14/off\"><button class=\"button button2\">OFF</button></a></p>");
-            }
-            client.println("</body></html>");
             
             // The HTTP response ends with another blank line
             client.println();
